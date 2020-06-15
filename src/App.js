11475666c825d5af5c413d70f26310b20b3a8020
Rafa <rafaeloliveiraduarte@hotmail.com>
2020-06-15 12:37:17 -0300

    Setup do Materialize CSS.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'react-materialize';
 
 function App() {
   return (
     <div className="App">
       <h1>Setup Inicial</h1>
+      <Button>button</Button>
     </div>
   );
 }
